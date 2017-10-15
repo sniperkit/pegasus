8969f31e291bbc1240fa824e3fa1d6dbed68e863
christospapidas <cpapidas@gmail.com>
2017-10-15 15:05:56 +0300

    Refactor netamqp publishing struct

--- network/netamqp/publishing.go
+++ network/netamqp/publishing.go
@@ -47,57 +47,103 @@ func (p *publishing) setParams(params map[string]string) {
 
 // configurePublishing configures the publishing method, assigning the headers value in publishing properties.
 func (p *publishing) configurePublishing() {
+	p.setContentType()
+	p.setContent()
+	p.setPriority()
+	p.setCorrelation()
+	p.setDelivery()
+	p.setReply()
+	p.setExpiration()
+	p.setMessage()
+	p.setTimestamp()
+	p.setType()
+	p.setUser()
+	p.setApp()
+}
 
+// setContentType set header
+func (p *publishing) setContentType() {
 	if p.optionHeaders["Content-Type"] != "" {
 		p.ContentType = p.optionHeaders["Content-Type"]
 	}
+}
 
+// setContent set header
+func (p *publishing) setContent() {
 	if p.optionHeaders["MQ-Content-Encoding"] != "" {
 		p.ContentEncoding = p.optionHeaders["MQ-Content-Encoding"]
 	}
+}
 
+// setDelivery set header
+func (p *publishing) setDelivery() {
 	if p.optionHeaders["MQ-Delivery-Mode"] != "" {
 		v, _ := strconv.Atoi(p.optionHeaders["MQ-Delivery-Mode"])
 		p.DeliveryMode = uint8(v)
 	}
+}
 
+// setPriority set header
+func (p *publishing) setPriority() {
 	if p.optionHeaders["MQ-Priority"] != "" {
 		v, _ := strconv.Atoi(p.optionHeaders["MQ-Priority"])
 		p.Priority = uint8(v)
 	}
+}
 
+// setCorrelation set header
+func (p *publishing) setCorrelation() {
 	if p.optionHeaders["MQ-Correlation-Id"] != "" {
 		p.CorrelationId = p.optionHeaders["MQ-Correlation-Id"]
 	}
+}
 
+// setReply set header
+func (p *publishing) setReply() {
 	if p.optionHeaders["MQ-Reply-To"] != "" {
 		p.ReplyTo = p.optionHeaders["MQ-Reply-To"]
 	}
+}
 
+// setExpiration set header
+func (p *publishing) setExpiration() {
 	if p.optionHeaders["MQ-Expiration"] != "" {
 		p.Expiration = p.optionHeaders["MQ-Expiration"]
 	}
+}
 
+// setMessage set header
+func (p *publishing) setMessage() {
 	if p.optionHeaders["MQ-Message-Id"] != "" {
 		p.MessageId = p.optionHeaders["MQ-Message-Id"]
 	}
+}
 
+// setTimestamp set header
+func (p *publishing) setTimestamp() {
 	if p.optionHeaders["MQ-Timestamp"] != "" {
 		layout := "2006-01-02T15:04:05.000Z"
 		t, _ := time.Parse(layout, p.optionHeaders["MQ-Timestamp"])
 		p.Timestamp = t
 	}
+}
 
+// setType set header
+func (p *publishing) setType() {
 	if p.optionHeaders["MQ-Type"] != "" {
 		p.Type = p.optionHeaders["MQ-Type"]
 	}
+}
 
+// setUser set header
+func (p *publishing) setUser() {
 	if p.optionHeaders["MQ-User-Id"] != "" {
 		p.UserId = p.optionHeaders["MQ-User-Id"]
 	}
+}
 
+func (p *publishing) setApp() {
 	if p.optionHeaders["MQ-App-Id"] != "" {
 		p.AppId = p.optionHeaders["MQ-App-Id"]
 	}
-
 }
