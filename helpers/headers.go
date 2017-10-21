bbe8f2965974097049385684513e4e2ff4189bd8
christospapidas <cpapidas@gmail.com>
2017-10-21 12:29:43 +0300

    Fix helpers headers

--- helpers/headers.go
+++ helpers/headers.go
@@ -13,7 +13,7 @@ const (
 
 // IsHTTPValidHeader returns true if the key is less than 3 characters or is not equal to GRPCHeaderKey & AMQPHeaderKey
 func IsHTTPValidHeader(key string) bool {
-	if key[0:3] != GRPCHeaderKey && key[0:3] != AMQPHeaderKey && key[0:3] != AMQPPramKey {
+	if len(key) > 3 && key[0:3] != GRPCHeaderKey && key[0:3] != AMQPHeaderKey && key[0:3] != AMQPPramKey {
 		return true
 	}
 	return false
@@ -21,7 +21,7 @@ func IsHTTPValidHeader(key string) bool {
 
 // IsGRPCValidHeader returns true if the key is less than 3 characters or is not equal to HTTPHeaderKey & AMQPHeaderKey
 func IsGRPCValidHeader(key string) bool {
-	if key[0:3] != HTTPHeaderKey && key[0:3] != AMQPHeaderKey && key[0:3] != AMQPPramKey {
+	if len(key) > 3 && key[0:3] != HTTPHeaderKey && key[0:3] != AMQPHeaderKey && key[0:3] != AMQPPramKey {
 		return true
 	}
 	return false
@@ -29,7 +29,7 @@ func IsGRPCValidHeader(key string) bool {
 
 // IsAMQPValidHeader returns true if the key is less than 3 characters or is not equal to GRPCHeaderKey & HTTPHeaderKey
 func IsAMQPValidHeader(key string) bool {
-	if key[0:3] != GRPCHeaderKey && key[0:3] != HTTPHeaderKey && key[0:3] != AMQPPramKey {
+	if len(key) > 3 && key[0:3] != GRPCHeaderKey && key[0:3] != HTTPHeaderKey && key[0:3] != AMQPPramKey {
 		return true
 	}
 	return false
